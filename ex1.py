86b93e8699e9f6aa681165a147e98f347820b2c9
Edinei Boscheto <edinei.boscheto2000@gmail.com>
2025-08-11 13:45:45 -0300

    Meu primeiro commit

--- ex1.py
+++
@@ -1,7 +0,0 @@
-#1 - Escreva um script que leia o nome, idade e peso do usuário e imprima essas
-#informações na tela.
-nome = input("Digite seu nome:")
-idade = input("Digite sua idade:")
-peso =  float(input("Digite seu peso:"))
-
-print(f"Seu nome é {nome}, sua idade é {idade}, e seu peso é {peso} Kg")
--- ex10.py
+++
@@ -1,4 +0,0 @@
-#EX3 - Mostrar os números de 100 a 1
-
-for i in range(100, 0, -1): # começa em 100 e vai até maior que 0, decrementando de 1 em 1
-    print(i) #mostra os numeros
